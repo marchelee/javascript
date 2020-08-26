a42ab443d615bc821ac477490baf5cfcb57819fb
nekoolim <33506132+marchelee@users.noreply.github.com>
2020-08-26 13:20:42 +0700

    react-shopping-cart

---
+++ react-shopping-cart/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import MenuItem from './components/MenuItem';
+
+import './css/reset.css';
+import './css/style.css';
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ react-shopping-cart/src/components/MenuItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MenuItem() {
+  return (
+    <div class="menu-item">
+      <span class="menu-text">Mouse - $199</span>
+      <span class="menu-action">
+        <button>+</button>
+      </span>
+    </div>
+  );
+}
+
+export default MenuItem;
